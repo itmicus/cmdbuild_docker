e30fea6e4d567b6c65d70f08689afe73930212b9
Itmicus <p.kuznetsov@itmicus.com>
2023-08-06 15:13:48 +0300

    fix db with postgis:12-3.3-alpine

--- docker-build.sh
+++ docker-build.sh
@@ -26,3 +26,4 @@ docker build -t itmicus/cmdbuild:om-2.0-3.2.1 openmaint-2.0-3.2.1/. --label "ver
 docker build -t itmicus/cmdbuild:om-2.0-3.2 openmaint-2.0-3.2/. --label "version=1.0" --label "maintaner=Itmicus <order@itmicus.com>"
 docker build -t afcarvalho1991/cmdbuild:om-2.1-3.3-b openmaint-2.1-3.3-b/. --label "version=1.0" --label "maintaner=Andre Carvalho <afccarvalho.1991@gmail.com>"
 docker build -t afcarvalho1991/cmdbuild:om-2.3-3.4.1-d openmaint-2.3-3.4.1-d/. --label "version=1.0" --label "maintaner=Andre Carvalho <afccarvalho.1991@gmail.com>"
+docker build -t itmicus/cmdbuild:om-2.3-3.4.1-d openmaint-2.3-3.4.1-d/. --label "version=1.0" --label "maintaner=Andre Carvalho <afccarvalho.1991@gmail.com>"
